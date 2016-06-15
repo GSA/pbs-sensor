c3dd2f9b0f5df976141871674a4e00eb445a5dcc
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2016-06-15 17:05:37 -0400

    replace backbone/backgrid with datatables.net for simplicity

--- js/rooms.js
+++ js/rooms.js
@@ -1,50 +1,41 @@
-var Room = Backbone.Model.extend({
-  defaults: {
-    number: 0,
-    temperature: 0
-  }
-});
-
-var RoomList = Backbone.Collection.extend({
-  model: Room,
-  comparator: "number",
-  temperatureRange: function(min, max) {
-    /* TODO: Make temperatureRange chainable by returning a Collection */
-    return this.filter(function (room) {
-      var temperature = room.get("temperature");
-      return (temperature >= min && temperature <= max);
-    });
-  }
-});
-
-var rooms = new RoomList();
-
-var columns = [{
-  name: "number",
-  label: "Room Number",
-  editable: false,
-  sortable: false,
-  cell: "string"
-}, {
-  name: "temperature",
-  label: "Temperature",
-  editable: false,
-  sortable: false,
-  cell: "integer"
-}];
+var originalRoomData = [];
+var filteredRoomData = [];
+var roomTable;
 
 function updateTemperatureRange(min, max) {
   $("#temperature-range").val(min + " - " + max);
-  var roomGrid = new Backgrid.Grid({
-    columns: columns,
-    emptyText: "No rooms match your preferences.",
-    className: "table table-striped",
-    collection: rooms.temperatureRange(min, max)
+  filteredRoomData = originalRoomData.filter(function (room) {
+    return (room.temperature >= min && room.temperature <= max);
   });
-  $("#room-list").html(roomGrid.render().el);
+  updateTable(filteredRoomData);
+}
+
+function updateTable(roomData) {
+  $("#room-list").DataTable({
+    data: roomData,
+    destroy: true,
+    searching: false,
+    lengthChange: false,
+    pageLength: 20,
+    columns: [
+      {
+        data: "number",
+        render: function (data) {
+          return '<a href="room.html">' + data + '</a>';
+        }
+      },
+      { data: "temperature" }
+    ]
+  });
 }
 
 $(document).ready(function () {
+  $.get("js/roomData.json", function (data, status) {
+    originalRoomData = data;
+    updateTable(originalRoomData);
+    updateTemperatureRange($("#slider-temperature-range").slider("values", 0), $("#slider-temperature-range").slider("values", 1));
+  });
+
   $("#slider-temperature-range").slider({
     range: true,
     min: 60,
@@ -54,16 +45,4 @@ $(document).ready(function () {
       updateTemperatureRange(ui.values[0], ui.values[1]);
     }
   });
-
-  rooms.fetch({
-    url: "js/roomData.json",
-    success: function () {
-      updateTemperatureRange($("#slider-temperature-range").slider("values", 0), $("#slider-temperature-range").slider("values", 1));
-    },
-    error: function () {
-      console.log("load fail");
-    }
-  });
-
-  updateTemperatureRange($("#slider-temperature-range").slider("values", 0), $("#slider-temperature-range").slider("values", 1));
 });
