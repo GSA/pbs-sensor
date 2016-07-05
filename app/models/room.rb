f87c946845533f3735fb5122b86aa172cd2d4cc8
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2016-07-05 17:22:25 -0400

    room model should be able to calculate its own sensor reading averages

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,7 @@
 class Room < ApplicationRecord
   has_many :readings
+
+  def update_averages
+    self.update(average_temperature: readings.compact.collect { |r| r.temperature.to_f }.sum / readings.compact.count) if readings.compact.any?
+  end
 end
