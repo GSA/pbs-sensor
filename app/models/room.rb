254ddea8066952db4797bec7337f9925602cb3ea
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2016-07-20 00:21:35 -0400

    add average updates for sound and co2

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,6 +2,11 @@ class Room < ApplicationRecord
   has_many :readings
 
   def update_averages
-    self.update(average_temperature: readings.compact.collect { |r| r.temperature.to_f }.sum / readings.compact.count) if readings.compact.any?
+    valid_readings = readings.compact
+    self.update(
+      average_temperature: valid_readings.collect { |r| r.temperature.to_f }.sum / valid_readings.count,
+      average_sound: valid_readings.collect { |r| r.sound.to_f }.sum / valid_readings.count,
+      average_co2: valid_readings.collect { |r| r.co2.to_f }.sum / valid_readings.count
+    ) if valid_readings.any?
   end
 end
