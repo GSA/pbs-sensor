2f9e370363c77e7e3d0b981818cf142f9eedcd8e
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2016-08-12 16:47:23 -0400

    clean up room views

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,3 +1,7 @@
 class Building < ApplicationRecord
   has_many :rooms
+
+  def to_s
+    name
+  end
 end
--- app/models/room_type.rb
+++ app/models/room_type.rb
@@ -1,3 +1,7 @@
 class RoomType < ApplicationRecord
   has_many :rooms
+  
+  def to_s
+    name
+  end
 end
